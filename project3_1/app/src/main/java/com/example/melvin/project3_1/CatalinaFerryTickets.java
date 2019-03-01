55d11c703bfabca70a8487ea2d94473fb5d3d2b5
Melvin Almonte <melvinalmonte@gmail.com>
2019-03-01 15:58:15 -0500

    added project 03/01

---
+++ project3_1/app/src/main/java/com/example/melvin/project3_1/CatalinaFerryTickets.java
@@ -0,0 +1,53 @@
+package com.example.melvin.project3_1;
+
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.os.*;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
+
+public class CatalinaFerryTickets extends AppCompatActivity{
+    String[] ferryRoutes = new String[]{"To Catalina Island", "To Long Beach"};
+    private Button renderBtn;
+    EditText handleData;
+    TextView displayData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//      RENDER SPINNER TO DEVICE
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalina_ferry_tickets);
+        final Spinner spin=findViewById(R.id.mySpinner);
+        final ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, ferryRoutes);
+        spin.setAdapter(myAdapter);
+
+        renderBtn=findViewById(R.id.button);
+        displayData=findViewById(R.id.textView);
+        displayData.setMovementMethod(new ScrollingMovementMethod());
+
+        renderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleData=findViewById(R.id.editInput);
+//                int ticketAmount = Integer.parseInt(handleData.getText().toString());
+//                int total;
+//                if (ferryRoutes[0] == "To Catalina Island"){
+//                    total = ticketAmount * 34;
+//                    displayData.setText(Integer.toString(total));
+//                }
+//                else{
+//                    total = ticketAmount * 18;
+//                    displayData.setText(Integer.toString(total));
+//                }
+                Log.i("DropDown","onClick: ");
+
+            }
+        });
+
+    }
+
+
+
+}
